e3ce36b9e801c1a49b518b72f0040a3ae9b1465b
Barry-owino <barryowino5@gmail.com>
2022-04-18 20:40:06 +0300

    prevent default submitting of the form

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", event => {
+  event.preventDefault();
   const username = document.querySelector("[name='username']").value.trim();
   const email = document.querySelector("[name='email']").value.trim();
   const password = document.querySelector("[name='password']").value.trim();
@@ -16,5 +17,5 @@ form.addEventListener("submit", event => {
     errors.push("Password can't be blank");
   if (password !== confirmPassword)
     errors.push("Passwords must match");
-
+  console.log(errors)
 });
